c323b3ba4728eac594616a6cae953e214138fdef
My Name <c.uwicyeza@alustudent.com>
2024-09-26 19:25:51 +0200

    LivingRoom

---
+++ alu-0x0A-unity-360_video_tour/Assets/Scripts/FlipNormals.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipNormals : MonoBehaviour
+{
+    public GameObject objectToFlip;
+
+    void Awake()
+    {
+        InvertSphere();
+    }
+
+    void InvertSphere()
+    {
+        Vector3[] normals = objectToFlip.GetComponent<MeshFilter>().mesh.normals;
+        for (int i = 0; i < normals.Length; i++)
+        {
+            normals[i] = -normals[i];
+        }
+        objectToFlip.GetComponent<MeshFilter>().sharedMesh.normals = normals;
+
+        int[] triangles = objectToFlip.GetComponent<MeshFilter>().sharedMesh.triangles;
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            int t = triangles[i];
+            triangles[i] = triangles[i + 2];
+            triangles[i + 2] = t;
+        }
+
+        objectToFlip.GetComponent<MeshFilter>().sharedMesh.triangles = triangles;
+    }
+}
---
+++ alu-0x0A-unity-360_video_tour/Assets/Scripts/MoveObjectM.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjectM : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    private Vector3 lastMousePosition;
+
+    void Update()
+    {
+        RotateObject();
+    }
+
+    void RotateObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentMousePosition = Input.mousePosition;
+            float deltaY = currentMousePosition.y - lastMousePosition.y;
+            transform.Rotate(0, deltaY * rotationSpeed, 0);
+            lastMousePosition = currentMousePosition;
+        }
+    }
+}
